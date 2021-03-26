d07e6e0bf33ad55e169404f15cc35d920c3859b9
R-Mielamud <2m.roman2@gmail.com>
2021-03-26 16:25:04 +0200

    Fixed sockets base

--- SuperFarmer/settings.py
+++ SuperFarmer/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "sockets",
     "authorization",
     "game",
 ]
@@ -121,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS
+
+SOCKETIO_CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://amritb.github.io",
+]
--- SuperFarmer/socket.py
+++ SuperFarmer/socket.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 from socketio import Server
 from SuperFarmer import settings
 
-io = Server(logger=settings.DEBUG)
+io = Server(logger=settings.DEBUG, cors_allowed_origins=settings.SOCKETIO_CORS_ALLOWED_ORIGINS)
