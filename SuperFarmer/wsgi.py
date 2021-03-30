1aec1c91417bbeef6297e5017a81dbbf24d07d8e
R-Mielamud <2m.roman2@gmail.com>
2021-03-30 21:43:32 +0300

    refactor sockets

--- SuperFarmer/wsgi.py
+++ SuperFarmer/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from socketio import WSGIApp
-from .socket import io
+from socket import io
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuperFarmer.settings')
 
